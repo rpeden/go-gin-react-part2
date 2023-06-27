6d7a1051d4436a853bb2b23471f9e2f8a4548f20
Ryan Peden <rpeden@users.noreply.github.com>
2023-06-27 05:22:14 -0400

    Updated components

--- chat-ui/src/MainChat.js
+++ chat-ui/src/MainChat.js
@@ -6,23 +6,23 @@ import MessagesPanel from './MessagesPanel';
 const MainChat = () => {
     const { channelId } = useParams();
     const navigate = useNavigate();
-    const [selectedChannel, setSelectedChannel] = useState(channelId || null);
+    const [selectedChannel, setSelectedChannel] = useState(parseInt(channelId) || null);
 
+    // If the component loads with a channel ID in the URL, set it as the selected channel.
     useEffect(() => {
-        if (channelId) {
-            setSelectedChannel(channelId);
+        if (selectedChannel) {
+            navigate(`/chat/${selectedChannel.id}`);
         }
-    }, [channelId]);
+    }, [selectedChannel, navigate]);
 
     const handleChannelSelect = (channelId) => {
         setSelectedChannel(channelId);
-        navigate(`/chat/${channelId}`);
     };
 
     return (
         <div className="flex h-screen">
             <div className="w-1/4 border-r">
-                <ChannelsList selectedChannel={selectedChannel} onChannelSelect={handleChannelSelect} />
+                <ChannelsList selectedChannel={selectedChannel} setSelectedChannel={handleChannelSelect} />
             </div>
             <div className="w-3/4">
                 <MessagesPanel selectedChannel={selectedChannel} />
